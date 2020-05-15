48aaa55a1c54853665237b6150c617778d3f75eb
wojtek_ubuntu <wojtek@ubuntu.com>
2020-05-15 23:35:50 +0200

    Init cw01/zad1

---
+++ cw01/zad1/myLibrary.c
@@ -0,0 +1,5 @@
+//
+// Created by wojtek on 15.05.2020.
+//
+
+#include "myLibrary.h"
---
+++ cw01/zad1/myLibrary.h
@@ -0,0 +1,8 @@
+//
+// Created by wojtek on 15.05.2020.
+//
+
+#ifndef SYSOPY_MYLIBRARY_H
+#define SYSOPY_MYLIBRARY_H
+
+#endif //SYSOPY_MYLIBRARY_H
