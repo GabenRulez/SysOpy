d20f7588add22de414ea91f290245bb10d6d5029
wojtek_ubuntu <wojtek@ubuntu.com>
2020-05-18 17:09:45 +0200

    Cmake + intellij

---
+++ cmake-build-debug/CMakeFiles/sysopy.dir/DependInfo.cmake
@@ -0,0 +1,21 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "C"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_C
+  "/home/wojtek/CLionProjects/SysOpy/cw01/zad1/myLibrary.c" "/home/wojtek/CLionProjects/SysOpy/cmake-build-debug/CMakeFiles/sysopy.dir/cw01/zad1/myLibrary.c.o"
+  "/home/wojtek/CLionProjects/SysOpy/main.c" "/home/wojtek/CLionProjects/SysOpy/cmake-build-debug/CMakeFiles/sysopy.dir/main.c.o"
+  )
+set(CMAKE_C_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_C_TARGET_INCLUDE_PATH
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ cmake-build-debug/CMakeFiles/sysopy.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/sysopy.dir/cw01/zad1/myLibrary.c.o"
+  "CMakeFiles/sysopy.dir/main.c.o"
+  "sysopy"
+  "sysopy.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/sysopy.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
