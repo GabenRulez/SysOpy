29146a111d75578771a251733397a628d7891f32
wojtek_linux <wojtek_linux@mail.com>
2020-06-05 23:33:24 +0200

    Lab6 init

--- cmake-build-debug/CMakeFiles/sysopy.dir/DependInfo.cmake
+++ cmake-build-debug/CMakeFiles/sysopy.dir/DependInfo.cmake
@@ -4,9 +4,8 @@ set(CMAKE_DEPENDS_LANGUAGES
   )
 # The set of files for implicit dependencies of each language:
 set(CMAKE_DEPENDS_CHECK_C
-  "/home/wojtek/CLionProjects/SysOpy/cw05/zad3/konsument.c" "/home/wojtek/CLionProjects/SysOpy/cmake-build-debug/CMakeFiles/sysopy.dir/cw05/zad3/konsument.c.o"
-  "/home/wojtek/CLionProjects/SysOpy/cw05/zad3/main.c" "/home/wojtek/CLionProjects/SysOpy/cmake-build-debug/CMakeFiles/sysopy.dir/cw05/zad3/main.c.o"
-  "/home/wojtek/CLionProjects/SysOpy/cw05/zad3/producent.c" "/home/wojtek/CLionProjects/SysOpy/cmake-build-debug/CMakeFiles/sysopy.dir/cw05/zad3/producent.c.o"
+  "/home/wojtek/CLionProjects/SysOpy/cw06/zad1/klient.c" "/home/wojtek/CLionProjects/SysOpy/cmake-build-debug/CMakeFiles/sysopy.dir/cw06/zad1/klient.c.o"
+  "/home/wojtek/CLionProjects/SysOpy/cw06/zad1/serwer.c" "/home/wojtek/CLionProjects/SysOpy/cmake-build-debug/CMakeFiles/sysopy.dir/cw06/zad1/serwer.c.o"
   )
 set(CMAKE_C_COMPILER_ID "GNU")
 
--- cmake-build-debug/CMakeFiles/sysopy.dir/cmake_clean.cmake
+++ cmake-build-debug/CMakeFiles/sysopy.dir/cmake_clean.cmake
@@ -1,7 +1,6 @@
 file(REMOVE_RECURSE
-  "CMakeFiles/sysopy.dir/cw05/zad3/konsument.c.o"
-  "CMakeFiles/sysopy.dir/cw05/zad3/main.c.o"
-  "CMakeFiles/sysopy.dir/cw05/zad3/producent.c.o"
+  "CMakeFiles/sysopy.dir/cw06/zad1/klient.c.o"
+  "CMakeFiles/sysopy.dir/cw06/zad1/serwer.c.o"
   "sysopy"
   "sysopy.pdb"
 )
